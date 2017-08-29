f9b6f7d51392f9c12e9d4799802a4c1c435c0b02
mzurbano <chyly_25@hotmail.com>
2017-08-28 23:00:31 -0300

    +User editor component

--- LELNg/src/app/pages/users/users.module.ts
+++ LELNg/src/app/pages/users/users.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { UserEditorComponent } from './user-editor/user-editor.component';
 import { MdCardModule , MdButtonModule, MdIconModule } from '@angular/material';
+import {MdInputModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MdCardModule,
     MdButtonModule,
-    MdIconModule
+    MdIconModule,
+    MdInputModule
   ],
   declarations: [UsersComponent, UserEditorComponent]
 })
