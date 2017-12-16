01bc9ecf0527cd569869d4f456ce2bae81ef57a2
mzurbano <chyly_25@hotmail.com>
2017-12-16 16:33:31 -0300

    symbol editor

--- LELNg/src/app/app.routing.ts
+++ LELNg/src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { SymbolsComponent } from './pages/symbols/symbols.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UsersComponent } from './pages/users/users.component';
 import { UserEditorComponent } from './pages/users/user-editor/user-editor.component';
+import { SymbolEditorComponent } from './pages/symbols/symbol-editor/symbol-editor.component';
 
 const ROUTES: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full', data: [{ title: 'Dashboard' }] },
@@ -11,7 +12,8 @@ const ROUTES: Routes = [
     { path: 'lelprojects', component: LelProjectsComponent, data: [{ title: 'Projects' }] },
     { path: 'symbols', component: SymbolsComponent, data: [{ title: 'Symbols' }] } ,
     { path: 'users', component: UsersComponent, data: [{ title: 'Users' }] },
-    { path: 'user/:id', component: UserEditorComponent}
+    { path: 'user/:id', component: UserEditorComponent},
+    { path: 'symbol/:id', component: SymbolEditorComponent},
 ];
 
 export const routing = RouterModule.forRoot(ROUTES);
--- LELNg/src/app/pages/symbols/symbols.module.ts
+++ LELNg/src/app/pages/symbols/symbols.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SymbolsComponent } from './symbols.component';
 import { MdListModule } from '@angular/material';
+import { SymbolEditorComponent } from './symbol-editor/symbol-editor.component';
+import { MdCardModule , MdButtonModule } from '@angular/material';
+import {MdInputModule} from '@angular/material';
+import {MdSelectModule} from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    MdListModule
+    MdListModule,
+    MdCardModule,
+    MdButtonModule,
+    MdInputModule,
+    MdSelectModule
   ],
-  declarations: [SymbolsComponent]
+  declarations: [SymbolsComponent, SymbolEditorComponent]
 })
 export class SymbolsModule { }
