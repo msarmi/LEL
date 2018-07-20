9af8dd8164d311b9b254afcbd65ecd0cd916a4ec
Mariela Zurbano <mzurbano@hexacta.com>
2018-07-19 23:30:14 -0300

    Tooltip

--- LELNg/src/app/shared/modules/material.module.ts
+++ LELNg/src/app/shared/modules/material.module.ts
@@ -6,14 +6,15 @@ import { MatExpansionModule } from '@angular/material';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
     imports: [MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatGridListModule,
         MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule, MatListModule, MatProgressSpinnerModule,
-        MatSelectModule, MatDialogModule, MatChipsModule, MatExpansionModule, MatAutocompleteModule ],
+        MatSelectModule, MatDialogModule, MatChipsModule, MatExpansionModule, MatAutocompleteModule, MatTooltipModule ],
     exports: [MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatGridListModule,
         MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule, MatListModule, MatProgressSpinnerModule,
-        MatSelectModule, MatDialogModule, MatChipsModule, MatExpansionModule, MatAutocompleteModule ]
+        MatSelectModule, MatDialogModule, MatChipsModule, MatExpansionModule, MatAutocompleteModule, MatTooltipModule ]
 })
 
 export class MaterialModule { }
